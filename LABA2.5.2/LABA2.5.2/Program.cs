9ec434b805a992679279f632618e593e7c24e880
asiansouldja <60935812+asiansouldja@users.noreply.github.com>
2020-04-25 20:35:44 +0300

    Task fifth2
    fifth2

---
+++ LABA2.5.2/LABA2.5.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LABA2._5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Random random = new Random();
+            Console.Write("Enter n:");
+            n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter m:");
+            m = Int32.Parse(Console.ReadLine());
+            int[,] x = new int[n, m];
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    x[i, j] = random.Next(-100, 100);
+                    Console.WriteLine(x[i, j]);
+                }
+            }
+
+
+            double sum = 0;
+
+
+            for (int i = 0; i < m; ++i)
+            {
+
+
+                for (int j = 0; j < n; ++j)
+                {
+
+
+                    if (x[i, j] < 0)
+                    {
+                        sum += x[i, j];
+                    }
+
+                }
+
+
+            }
+            Console.WriteLine("Sum of negative:" + sum);
+        }
+    }
+}
